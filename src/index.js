6f9a3acf0fb45d9245da827a6947c1990bd90995
Paula Jach <paulajach92@gmail.com>
2020-02-14 16:01:26 +0100

    added folder with psd file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 import '../scss/main.scss';
 
-// const icon = document.querySelector('.menu--icon');
-// const navbar = document.querySelector('.navbar--mobile');
-// const menuList = document.querySelector('.navbar--nav__link');
-// // hamburger.addEventListener('click', function(hamburger){
-// //     hamburger.classList.toggle('change');
-// //     console.log('clicked');
-// // });
 
-// // icon.addEventListener('click', function() {
-// //     console.log('clicked');
-// //     menuList.classList.add('hide');
-// // });
-
-// icon.addEventListener('click', () => {
-//     const menuList = document.querySelector('.navbar--nav__link');
-//     const icon = document.querySelector('.menu--icon');
-//     if(menuList.style.display === 'inline-block'){
-//         menuList.style.display === 'none';
-//     } else {
-//         menuList.style.display === 'inline-block';
-//     }
-// });
 
